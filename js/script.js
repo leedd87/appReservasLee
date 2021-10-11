7c42d2d784d9b34c840c0bbfe085b95e94c2d374
Lee David <leedav87@gmail.com>
2021-10-10 23:00:27 -0300

    mejora de app

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const reserva = [];
 
+
 //METODO READY()
 $(() => {
 
@@ -13,71 +14,78 @@ $(() => {
 
     //SELECCION DE HOTEL
 
-    $('#btnPuntaCana').click((e) => {
-        $('#mostrarHabitaciones').fadeIn('slow');
-        seleccionPuntaCana();
-        reserva.push('Hotel Punta Cana')
-        localStorage.setItem('reserva', JSON.stringify(reserva));
-    });
+    // $('#btnPuntaCana').one("click", (e) => {
+    //     $('#mostrarHabitaciones').fadeIn('slow');
+    //     seleccionPuntaCana();
 
-    $('#btnDominica').click((e) => {
-        $('#mostrarHabitaciones').fadeIn('slow');
-        seleccionDominica();
-        reserva.push('Hotel Dominica')
-        localStorage.setItem('reserva', JSON.stringify(reserva));
-    });
+    //     //ARMAR FUNCION PARA DESAPARECER
+    //     $('#hotelPuntaCana').fadeOut('slow');
+    //     $('#hotelDominica').fadeOut('slow');
+    //     $('#hotelGrandBavaro').fadeOut('slow');
+    //     //ARMAR FUNCION PARA DESAPARECER
 
-    $('#btnGrandBavaro').click((e) => {
-        $('#mostrarHabitaciones').fadeIn('slow');
-        seleccionGrandBavaro();
-        reserva.push('Hotel Grand Bavaro')
-        localStorage.setItem('reserva', JSON.stringify(reserva));
-    });
+    //     reserva.push('Hotel Punta Cana')
+    //     localStorage.setItem('reserva', JSON.stringify(reserva));
+    // });
 
-    //SELECCION DE HABITACION
+    // $('#btnDominica').click((e) => {
+    //     $('#mostrarHabitaciones').fadeIn('slow');
+    //     seleccionDominica();
+    //     reserva.push('Hotel Dominica')
+    //     localStorage.setItem('reserva', JSON.stringify(reserva));
+    // });
 
-    $('#btnHabitacionSuite').click((e) => {
-        $('#mostrarServicios').fadeIn('slow', () => ($('#mostrarHabitaciones').fadeOut('slow')));
-        seleccionSuite()
-        reserva.push('Habitacion Suite')
-        localStorage.setItem('reserva', JSON.stringify(reserva))
-    });
+    // $('#btnGrandBavaro').click((e) => {
+    //     $('#mostrarHabitaciones').fadeIn('slow');
+    //     seleccionGrandBavaro();
+    //     reserva.push('Hotel Grand Bavaro')
+    //     localStorage.setItem('reserva', JSON.stringify(reserva));
+    // });
 
-    $('#btnHabitacionDoble').click((e) => {
-        $('#mostrarServicios').fadeIn('slow', () => ($('#mostrarHabitaciones').fadeOut('slow')));
-        seleccionDoble()
-        reserva.push('Habitacion Doble')
-        localStorage.setItem('reserva', JSON.stringify(reserva))
-    });
+    // //SELECCION DE HABITACION
 
-    $('#btnHabitacionTriple').click((e) => {
-        $('#mostrarServicios').fadeIn('slow', () => ($('#mostrarHabitaciones').fadeOut('slow')));
-        seleccionTriple();
-        reserva.push('Habitacion Triple')
-        localStorage.setItem('reserva', JSON.stringify(reserva))
-    });
+    // $('#btnHabitacionSuite').click((e) => {
+    //     $('#mostrarServicios').fadeIn('slow', () => ($('#mostrarHabitaciones').fadeOut('slow')));
+    //     seleccionSuite()
+    //     reserva.push('Habitacion Suite')
+    //     localStorage.setItem('reserva', JSON.stringify(reserva))
+    // });
 
-    //MUESTRA EN DOM
+    // $('#btnHabitacionDoble').click((e) => {
+    //     $('#mostrarServicios').fadeIn('slow', () => ($('#mostrarHabitaciones').fadeOut('slow')));
+    //     seleccionDoble()
+    //     reserva.push('Habitacion Doble')
+    //     localStorage.setItem('reserva', JSON.stringify(reserva))
+    // });
 
-    $('#btnAllInclusive').click((e) => {
-        seleccionAllInclusive();
-        reserva.push('Servicio All Inclusive')
-        localStorage.setItem('reserva', JSON.stringify(reserva))
-    });
+    // $('#btnHabitacionTriple').click((e) => {
+    //     $('#mostrarServicios').fadeIn('slow', () => ($('#mostrarHabitaciones').fadeOut('slow')));
+    //     seleccionTriple();
+    //     reserva.push('Habitacion Triple')
+    //     localStorage.setItem('reserva', JSON.stringify(reserva))
+    // });
 
-    $('#btnMediaPension').click((e) => {
-        $('#mostrarServicios').fadeOut('slow')
-        seleccionMediaPension();
-        reserva.push('Servicio Media Pension')
-        localStorage.setItem('reserva', JSON.stringify(reserva))
-    });
+    // //MUESTRA EN DOM
 
-    $('#btnSoloDesayuno').click((e) => {
-        $('#mostrarServicios').fadeOut('slow')
-        seleccionSoloDesayuno();
-        reserva.push('Servicio Solo Desayuno')
-        localStorage.setItem('reserva', JSON.stringify(reserva))
-    });
+    // $('#btnAllInclusive').click((e) => {
+    //     seleccionAllInclusive();
+    //     reserva.push('Servicio All Inclusive')
+    //     localStorage.setItem('reserva', JSON.stringify(reserva))
+    // });
+
+    // $('#btnMediaPension').click((e) => {
+    //     $('#mostrarServicios').fadeOut('slow')
+    //     seleccionMediaPension();
+    //     reserva.push('Servicio Media Pension')
+    //     localStorage.setItem('reserva', JSON.stringify(reserva))
+    // });
+
+    // $('#btnSoloDesayuno').click((e) => {
+    //     $('#mostrarServicios').fadeOut('slow')
+    //     seleccionSoloDesayuno();
+    //     reserva.push('Servicio Solo Desayuno')
+    //     localStorage.setItem('reserva', JSON.stringify(reserva))
+    // });
 
 
     //BTN RESSERVAR Y CANCEL
@@ -89,29 +97,44 @@ $(() => {
     $('#btnReservar').click((e) => {
         alert('Gracias por tu reserva!')
     });
+
+    //BTN ACTIVIDADES
+    $('#actividadesBtn').one("click", (e) => {
+        actividadesCard();
+
+    })
+
+    //BTN HOTELES
+    //CLICK ME
+    $('#hotelesDisponibles').click((e) => {
+        hotelesCard();
+    })
+
+    //BTN HABITACIONES
+    $("#habitacionesDisponibles").one("click", ((e) => {
+        habitacionesCard();
+    }))
+
+
+    $('.hola').click((e) => {
+        alert('jajajaja')
+    })
+
+    $('#habitacionDoble').click((e) => {
+        console.log('hello Universo')
+    })
+
+
+    $('#id1').click((e) => {
+        alert('jajajajaja')
+    })
+
+
 });
 
-//BTN ACTIVIDADES
-$('#button').click((e) => {
-    actividadesCard();
-})
+// let boton = document.querySelector("#habitacionSuite");
+
+// console.log(boton)
 
 
-function actividadesCard() {
-    $.get("./js/data.json", (data, status) => {
-        if (status === 'success') {
-            let setDatos = data;
-            for (let i = 0; i < data.actividades.length; i++) {
-                $('#actividades').append(
-                    `<div class="card text-center container align-items-center m-3 p-3 d-flex">
-                    <img src=${data.actividades[i].foto} class="card-img-top" alt="card-img-top">
-                    <div class="card-body">
-                    <h5 class="card-title">${data.actividades[i].nombre}</h5>
-                    <p class="card-text ">${data.actividades[i].texto}</p>
-                    </div>
-                    </div>`
-                )
-            }
-        } else { alert('No se pudieron cargar las actividades') }
-    })
-}
+
--- js/scripts_function.js
+++ js/scripts_function.js
@@ -1,39 +1,183 @@
-//SELECCION HOTEL
+let precio = 0;
 
-function seleccionPuntaCana() {
-    $('#seleccionHotel').append(`<p class='seleccion'>Elegiste Hotel Punta Cana</p>`);
-}
 
-function seleccionDominica() {
-    $('#seleccionHotel').append(`<p class='seleccion'>Elegiste Hotel Dominica</p>`);
-}
 
-function seleccionGrandBavaro() {
-    $('#seleccionHotel').append(`<p class='seleccion'>Elegiste Hotel Grand Bavaro</p>`)
-}
 
 //SELECCION HABITACION
 function seleccionSuite() {
-    $('#seleccionHabitacion').append(`<p class='seleccion'>Elegiste habitacion Suite</p>`)
+    $("#seleccionHabitacion").append(
+        `<p class='seleccion'>Elegiste habitacion Suite</p>`
+    );
 }
 
 function seleccionDoble() {
-    $('#seleccionHabitacion').append(`<p class='seleccion'>Elegiste habitacion Doble</p>`)
+    $("#seleccionHabitacion").append(
+        `<p class='seleccion'>Elegiste habitacion Doble</p>`
+    );
 }
 
 function seleccionTriple() {
-    $('#seleccionHabitacion').append(`<p class='seleccion'>Elegiste habitacion Triple</p>`)
+    $("#seleccionHabitacion").append(
+        `<p class='seleccion'>Elegiste habitacion Triple</p>`
+    );
 }
 
 //SELECCION SERVICIO
 function seleccionAllInclusive() {
-    $('#seleccionServicio').append(`<p class='seleccion'>Elegiste el servicio de All Inclusive</p>`)
+    $("#seleccionServicio").append(
+        `<p class='seleccion'>Elegiste el servicio de All Inclusive</p>`
+    );
 }
 
 function seleccionMediaPension() {
-    $('#seleccionServicio').append(`<p class='seleccion'>Elegiste el servicio de Media Pension</p>`)
+    $("#seleccionServicio").append(
+        `<p class='seleccion'>Elegiste el servicio de Media Pension</p>`
+    );
 }
 
 function seleccionSoloDesayuno() {
-    $('#seleccionServicio').append(`<p class='seleccion'>Elegiste el servicio de solo Desayuno</p>`)
+    $("#seleccionServicio").append(
+        `<p class='seleccion'>Elegiste el servicio de solo Desayuno</p>`
+    );
+}
+
+function actividadesCard() {
+    $.get("./js/data.json", (data, status) => {
+        if (status === "success") {
+            let setDatos = data;
+            for (let i = 0; i < data.actividades.length; i++) {
+                $("#actividades").append(
+                    `<div class="card text-center container align-items-center m-3 p-3 d-flex">
+                    <img src=${data.actividades[i].foto} class="card-img-top" alt="card-img-top">
+                    <div class="card-body">
+                    <h5 class="card-title">${data.actividades[i].nombre}</h5>
+                    <p class="card-text ">${data.actividades[i].texto}</p>
+                    </div>
+                    </div>`
+                );
+            }
+        } else {
+            alert("No se pudieron cargar las actividades");
+        }
+    });
+}
+
+function hotelesCard() {
+    $.get("./js/data.json", (data, status) => {
+        if (status === "success") {
+            let setDatos = data;
+            for (let i = 0; i < data.hotel.length; i++) {
+                $("#mostrarHoteles").append(
+                    `<div class="card text-center container align-items-center m-3 p-3 d-flex">
+                        <img src=${data.hotel[i].foto} class="card-img-top" alt="card-img-top">
+                        <div class="card-body">
+                        <h5 class="card-title">${data.hotel[i].nombre}</h5>
+                        <p class="card-text">${data.hotel[i].texto}</p>
+                        <button class="btn btn-primary deshotel" id="${data.hotel[i].id}">Pick Me</button>
+                        </div>
+                        </div>`
+                );
+            }
+            seleccionHotel(data.hotel);
+        } else {
+            alert("No se pudieron cargar los hoteles");
+        }
+    });
+}
+
+function seleccionHotel(listaHoteles) {
+    listaHoteles.forEach((element) => {
+        $(`#${element.id}`).click((e) => {
+            $("#seleccionHotel").append(
+                `<p class='seleccion'>Elegiste ${element.nombre}</p>`
+            );
+            $(".deshotel").prop("disabled", true);
+            habitacionesCard()
+        });
+    });
+};
+
+
+
+
+function habitacionesCard() {
+    $.get("./js/data.json", (data, status) => {
+        if (status === "success") {
+            let setDatos = data;
+            for (let i = 0; i < data.habitaciones.length; i++) {
+                $("#mostrarHabitaciones").append(
+                    `<div class="card text-center container align-items-center m-3 p-3 d-flex">
+                            <img src=${data.habitaciones[i].foto} class="card-img-top" alt="card-img-top">
+                            <div class="card-body">
+                            <h5 class="card-title">${data.habitaciones[i].nombre}</h5>
+                            <p class="card-text ">${data.habitaciones[i].texto}</p>
+                            <p id='precio' data-value="${data.habitaciones[i].precio}">Precio por noche: $ ${data.habitaciones[i].precio}</p>
+                            <button class="btn btn-primary deshabitacion" id="${data.habitaciones[i].id}">Pick Me</button>
+                            </div>
+                            </div>`
+                );
+            }
+            seleccionHabitacion(data.habitaciones);
+        } else {
+            alert("No se pudieron cargar las habitaciones");
+        }
+    })
+}
+
+function seleccionHabitacion(listaHabitaciones) {
+    listaHabitaciones.forEach((element) => {
+        $(`#${element.id}`).click((e) => {
+            $("#seleccionHabitacion").append(
+                `<p class='seleccion'>Elegiste ${element.nombre}</p>
+            <p class="seleccion">Precio de $<span id="precioHabitacion">${element.precio}</span>`
+            );
+            ServiciosCard()
+            $(".deshabitacion").prop("disabled", true);
+        })
+    })
+}
+
+function ServiciosCard() {
+    $.get("./js/data.json", (data, status) => {
+        if (status === "success") {
+            let setDatos = data;
+            for (let i = 0; i < data.servicios.length; i++) {
+                $("#mostrarServicios").append(
+                    `<div class="card text-center container align-items-center m-3 p-3 d-flex">
+                        <img src=${data.servicios[i].foto} class="card-img-top" alt="card-img-top">
+                        <div class="card-body">
+                        <h5 class="card-title">${data.servicios[i].nombre}</h5>
+                        <p class="card-text">${data.servicios[i].texto}</p>
+                        <p id='precio' data-value="${data.servicios[i].precio}">Precio por noche: $ ${data.servicios[i].precio}</p>
+                        <button class="btn btn-primary desservicios" id="${data.servicios[i].id}">Pick Me</button>
+                        </div>
+                        </div>`
+                );
+            }
+            seleccionServicios(data.servicios);
+        } else {
+            alert("No se pudieron cargar los hoteles");
+        }
+    });
+}
+
+function seleccionServicios(listaServicios) {
+    listaServicios.forEach((element) => {
+        $(`#${element.id}`).click((e) => {
+            $("#seleccionServicio").append(
+                `<p class='seleccion'>Elegiste ${element.nombre}</p>
+            <p class="seleccion">Precio de $<span id="precioServicio">${element.precio}</span>`
+            );
+            $(".desservicios").prop("disabled", true);
+            calcularEstadia()
+        })
+    })
+}
+
+function calcularEstadia() {
+
+    let precioHabitacion = document.querySelector('#precioHabitacion')
+    let precioServicio = document.querySelector('#precioServicio')
+    console.log(precioHabitacion);
+    console.log(precioServicio);
 }
